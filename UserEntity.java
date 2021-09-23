2bf55acf2a47b64691beba975f575add5c9c5d9b
dxl <986853314@qq.com>
2021-09-23 09:17:00 +0800

    2

--- UserEntity.java
+++ UserEntity.java
@@ -13,54 +13,7 @@ public class UserEntity {
     private int uflag;
     private RoleEntity uroleid;
 
-    public int getUid() {
-        return uid;
-    }
-
-    public void setUid(int uid) {
-        this.uid = uid;
-    }
-
-    public String getUname() {
-        return uname;
-    }
-
-    public void setUname(String uname) {
-        this.uname = uname;
-    }
-
-    public String getUpwd() {
-        return upwd;
-    }
-
-    public void setUpwd(String upwd) {
-        this.upwd = upwd;
-    }
-
-    public String getUphone() {
-        return uphone;
-    }
-
-    public void setUphone(String uphone) {
-        this.uphone = uphone;
-    }
-
-    public int getUflag() {
-        return uflag;
-    }
-
-    public void setUflag(int uflag) {
-        this.uflag = uflag;
-    }
-
-    public RoleEntity getUroleid() {
-        return uroleid;
-    }
-
-    public void setUroleid(RoleEntity uroleid) {
-        this.uroleid = uroleid;
-    }
-
+  
     @Override
     public String toString() {
         return "UserEntity{" +
